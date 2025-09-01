69f30899162be2f10cc9715d320fddbca986c3d8
AthiraPR-2025 <athirapadinjare.purakkalravi-extern@olympus.com>
2025-09-01 15:43:16 +0530

    Update common.php

--- app/classes/cums/common.php
+++ app/classes/cums/common.php
@@ -3,6 +3,53 @@ namespace Cums;
 
 class Common
 {
+    /**
+     * Pull the latest changes from the Git repo.
+     */
+    public static function gitPull(string $repoPath): void
+    {
+        if (!is_dir($repoPath)) {
+            \Log::error("Repo path not found: $repoPath");
+            return;
+        }
+
+        $output = shell_exec("cd " . escapeshellarg($repoPath) . " && git pull 2>&1");
+        \Log::info("Git pull executed at $repoPath: $output");
+    }
+
+    /**
+     * Find index.html files up to a given max depth.
+     */
+    public static function findLocalIndexHtmlFiles(string $baseDir, int $maxDepth = 3): array
+    {
+        $files = [];
+
+        $baseDir = realpath($baseDir);
+        if (!$baseDir || !is_dir($baseDir)) {
+            \Log::warning("Invalid base directory: {$baseDir}");
+            return $files;
+        }
+
+        $iterator = new \RecursiveIteratorIterator(
+            new \RecursiveDirectoryIterator($baseDir),
+            \RecursiveIteratorIterator::SELF_FIRST
+        );
+
+        foreach ($iterator as $file) {
+            if (!$file->isFile()) continue;
+            if ($file->getFilename() !== 'index.html') continue;
+
+            $depth = substr_count(str_replace($baseDir, '', $file->getPath()), DIRECTORY_SEPARATOR);
+            if ($depth <= $maxDepth) {
+                $files[] = $file->getPathname();
+            }
+        }
+
+        return $files;
+    }
+
+    // Optional: comment out SSH function
+    /*
     public static function ssh2_exec($session, string $command)
     {
         $stream = ssh2_exec($session, $command);
@@ -13,4 +60,5 @@ class Common
         fclose($stream);
         return $result;
     }
+    */
 }
