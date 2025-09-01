065c0829712e40a2437b587a18720319ec52317a
AthiraPR-2025 <athirapadinjare.purakkalravi-extern@olympus.com>
2025-09-01 15:45:12 +0530

    Update collectcontent.php

--- app/tasks/collectcontent.php
+++ app/tasks/collectcontent.php
@@ -15,54 +15,42 @@ class CollectContent
         try
         {
             Config::load('cums', true);
-            $remote_server = Config::get('cums.remote_server');
-            //echo var_export($remote_server, true) . PHP_EOL;
-
-            $session = ssh2_connect($remote_server['host']);
-            if ( ! ssh2_auth_password($session, $remote_server['user'], $remote_server['password']))
-            {
-                die('Authentication Failed...');
-            }
-
             $find = Config::get('cums.find');
 
+            // 1. Pull latest changes from local git repos for each domain
+            foreach (Domain::list(true) as $dir) {
+                Common::gitPull($dir['documentroot']);
+            }
+
             $result = [];
+
+            // 2. Scan index.html files locally (up to maxdepth)
             foreach (Domain::list(true) as $dir)
             {
-                $command = <<<END
-find {$dir['documentroot']} -maxdepth {$find['maxdepth']} -xtype f -name "index.html"
-END;
-                //echo $command . PHP_EOL;
-
-                $result[$dir['domainid']] = Common::ssh2_exec($session, $command);
+                $files = Common::findLocalIndexHtmlFiles($dir['documentroot'], $find['maxdepth']);
+                $result[$dir['domainid']] = $files;
             }
 
+            // 3. Append additional files from Directory table (paths > 3rd level)
             foreach (Directory::list_with_domain() as $dir)
             {
-                $path = rtrim($dir['documentroot'], '/') . '/' . trim($dir['path'], '/');
-                if ( ! strpos(basename($path), '.'))
-                {
-                    /*
-                     * ベース名に「.」を含む場合はファイルとみなし、そのファイルを探す。
-                     * ベース名に「.」を含まない場合はディレクトリとみなし、その直下の"index.html"ファイルを探す。
-                     */
-                    $path .= '/index.html';
+                $basePath = rtrim($dir['documentroot'], '/') . '/' . trim($dir['path'], '/');
+                if (strpos(basename($basePath), '.') === false) {
+                    // if no dot, treat as directory, add /index.html
+                    $basePath .= '/index.html';
                 }
 
-                $command = <<<END
-find {$path} -maxdepth 0 -xtype f
-END;
-                //echo $command . PHP_EOL;
-
-                $result[$dir['domainid']] .= Common::ssh2_exec($session, $command);
+                if (is_file($basePath)) {
+                    $result[$dir['domainid']][] = $basePath;
+                }
             }
-            //echo var_export($result, true) . PHP_EOL;
 
             $multi_parameters = [];
+
+            // 4. Read each file locally, extract title and file modification time
             foreach ($result as $domainid => $files)
             {
-                // array_filterで空行（最後の行）を削除
-                foreach (array_filter(explode("\n", str_replace(array("\r\n", "\r", "\n"), "\n", $files)), 'strlen') as $file)
+                foreach ($files as $file)
                 {
                     $parameters = [
                         'domainid' => $domainid,
@@ -71,41 +59,29 @@ END;
                         'apemp'    => 'system',
                     ];
 
-                    $command = <<<END
-cat $file
-END;
-                    //echo $command . PHP_EOL;
-
-                    $target_html = Common::ssh2_exec($session, $command);
+                    $target_html = file_get_contents($file);
                     $target_html = mb_convert_encoding($target_html, 'HTML-ENTITIES', 'auto');
 
                     $dom = new \DOMDocument;
                     @$dom->loadHTML($target_html);
                     $node = $dom->getElementsByTagName('title')->item(0);
-                    $parameters['title'] = isset($node->textContent) && strlen($node->textContent) ? $node->textContent : 'title無し';
+                    $parameters['title'] = ($node && strlen($node->textContent)) ? $node->textContent : 'title無し';
 
-                    $command = <<<END
-stat --format %y $file
-END;
-                    //echo $command . PHP_EOL;
+                    $parameters['contentupdatedate'] = date('Y-m-d H:i:s', filemtime($file));
 
-                    $result = Common::ssh2_exec($session, $command);
-                    $result = str_replace(array("\r\n", "\r", "\n"), '', $result);
-                    $parameters['contentupdatedate'] = substr($result, 0, strpos($result, '.'));
                     $multi_parameters[] = $parameters;
                 }
             }
-            //echo var_export($multi_parameters, true) . PHP_EOL;
+
+            // 5. Refresh content in DB
             Content::refresh($multi_parameters);
 
-            ssh2_disconnect($session);
         }
         catch(\Exception $e)
         {
             \Log::error(__FILE__ . ':' . __LINE__);
             \Log::error('$e->getCode():' . var_export($e->getCode(), true));
             \Log::error('$e->getMessage():' . var_export($e->getMessage(), true));
-            //throw $e;
             exit($e->getMessage() . PHP_EOL);
         }
     }
